95f4861a429c8183db63664a0383eb47fadc02c3
YevaVudz <6257449@cegepmontpetit.ca>
2024-05-23 11:51:29 -0400

    tp web final modification 19

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+const boutonsModale = document.querySelectorAll('.bouton_image');
+const modale = document.querySelector('.modale');
+
+
+for (let i = 0; i < boutonsModale.length; i++){
+    boutonsModale[i].addEventListener('click', openModale);
+    
+}
+
+function openModale(){
+    const chemin = this.previousElementSibling;
+    const image = document.createElement('img');
+
+    image.setAttribute('src', chemin);
+    console.log(chemin);
+    
+    image.classList.add('zoom');
+
+
+    modale.appendChild(image);
+    modale.classList.add('visible');
+
+    image.addEventListener('click', closeModale);
+}
+
+function closeModale(event){
+    modale.removeChild(event.target);
+    modale.classList.remove('visible');
+}
